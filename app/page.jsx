dd4801749ba063cc26bbab4c5b55a2efd84670e1
evidenciasponao <evidenciasponao@gmail.com>
2023-04-09 21:28:56 -0500

    Fetch Data

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
-function IndexPage() {
+
+async function fecthUsers() {
+  const res = await fetch("https://reqres.in/api/users")
+  const data = await res.json()
+  return data.data
+}
+
+async function IndexPage() {
+  const users = await fecthUsers();
+  console.log(users)
   return (
     <div>
       <h1>IndexPage</h1>
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   );
 }
---
+++ app/users/page.jsx
@@ -0,0 +1,9 @@
+function UsersPage() {
+  return (
+    <div>
+        <h1>UsersPage</h1>
+    </div>
+  );
+}
+
+export default UsersPage
