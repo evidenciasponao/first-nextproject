1e83133418d2f273a95ee5407ab54a07a080a547
evidenciasponao <evidenciasponao@gmail.com>
2023-04-09 20:40:03 -0500

    creating pages

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero vitae nam dolores. Esse, aperiam suscipit, obcaecati ab saepe nostrum illo iusto soluta et, mollitia dolores rerum. Molestias accusamus illum dolor et temporibus numquam hic voluptate, ad natus amet. Inventore aliquam error qui similique, suscipit hic, quasi aperiam voluptates iure quos voluptas tempore totam. Omnis, ipsum rerum. Eos iusto enim adipisci obcaecati totam corrupti ipsa minus ab dicta sequi harum, mollitia commodi eveniet illo, cum quam rerum. Vero nostrum repudiandae non. Saepe odio facilis minus repellat iste error sed. Doloremque magnam rerum ad in. Facilis odio saepe sequi? Iure, quasi accusamus!</p>
+    </div>
+  );
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit neque, voluptatem modi quasi aspernatur fugit, numquam reiciendis dolores non optio adipisci ad pariatur deleniti earum itaque quidem beatae esse incidunt. Voluptate id deserunt, enim labore sint ratione asperiores molestiae! Dolores tenetur ratione, sit amet explicabo modi dolorum nobis unde corrupti?</p>
+    </div>
+  );
+}
+
+export default ServicesPage
