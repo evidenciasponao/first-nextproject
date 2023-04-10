0dbe2d7bac8295e733723306439581b0a6c5fb88
evidenciasponao <evidenciasponao@gmail.com>
2023-04-09 23:00:47 -0500

    User Details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,7 +1,23 @@
-function UsersPage() {
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  return data.data
+}
+
+async function UsersPage({params}) {
+
+  const user = await getUser(params.id)
+
   return (
     <div>
-        <h1>Users Details</h1>
+      <h1>User Details</h1>
+      <div>
+        <img src={user.avatar} alt= {user.email} />
+        <div>
+          <h3>{user.id} {user.first_name} {user.last_name}</h3>
+          <p>{user.email}</p>
+        </div>
+      </div>
     </div>
   );
 }
